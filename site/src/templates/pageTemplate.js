47defe733023f988c6aa5ab509824a40f121d4aa
gaurangrshah <gaurang.r.shah@gmail.com>
2020-01-12 18:16:30 -0500

    working --data rendering

--- site/src/templates/pageTemplate.js
+++ site/src/templates/pageTemplate.js
@@ -4,30 +4,30 @@ import Layout from "../gatsby-theme-styleguide/components/layout"
 
 export default function pageTemplate(props) {
   const { pageContext, data } = props
-  const { contextPage, to, layout, sections } = pageContext
+  const { contextPage, to, layout, sections, content, links } = pageContext
 
   // console.log(pages.filter(page => page.page === contextPage))
   return (
     <Layout title={data.site.siteMetadata.title}>
       <pre>{JSON.stringify(pageContext, null, 2)}</pre>
       <Link to={`/404.html`}>404</Link>
+      <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+        <ul>
+          {content && content.map((data, index) => {
+            return <li key={`content_item_${index}`}>{data.item}</li>
+          })}
+        </ul>
+        <ul>
+          {links && links.map((item, index) => {
+            return (
+              <li key={`link_${index}`}>
+                <Link to={item.to}>{item.to}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div >
     </Layout>
-    // <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-    //   <ul>
-    //     {content.map((data, index) => {
-    //       return <li key={`content_item_${index}`}>{data.item}</li>
-    //     })}
-    //   </ul>
-    //   <ul>
-    //     {links.map((item, index) => {
-    //       return (
-    //         <li key={`link_${index}`}>
-    //           <Link to={item.to}>{item.to}</Link>
-    //         </li>
-    //       )
-    //     })}
-    //   </ul>
-    // </div >
   )
 }
 
