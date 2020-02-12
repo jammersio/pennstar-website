5919e26904600a44ded669d0a0a22a73f1940a9d
gaurangrshah <gaurang.r.shah@gmail.com>
2020-02-12 13:21:20 -0500

    updates meta

--- site/src/components/Meta/Meta.js
+++ site/src/components/Meta/Meta.js
@@ -1,21 +1,103 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Meta = () => (
-  <Helmet
-    meta={[
-      {
-        name: 'charset',
-        content: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
-      },
-    ]}
-  >
-    <script src="https://kit.fontawesome.com/900da20747.js" crossorigin="anonymous" />
-  </Helmet>
-);
 
-export default Meta;
+
+function Meta({ description, lang, meta, title }) {
+
+  // TODO: add logo to query and include in meta below.
+
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  title = title ? title : site.siteMetadata.title
+  // TODO: include logo
+  // const { logo } = site.siteMetadata
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: 'charset',
+          content: 'utf-8',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        },
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:image`,
+          // TODO: include logo
+          // content: logo,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+Meta.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+Meta.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default Meta
+
